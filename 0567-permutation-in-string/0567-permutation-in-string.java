3d634373c37477c07ae1cc31c2b90a0f5d703de3
Shantanu Marik <146924611+Shantanum0701@users.noreply.github.com>
2026-01-12 18:38:28 +0530

    Time: 4 ms (99.5%), Space: 44.3 MB (44.9%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        // Count frequency of s1
+        for (char c : s1.toCharArray()) {
+            freq1[c - 'a']++;
+        }
+
+        int windowSize = s1.length();
+
+        // Initial window
+        for (int i = 0; i < windowSize; i++) {
+            freq2[s2.charAt(i) - 'a']++;
+        }
+
+        if (matches(freq1, freq2)) return true;
+
+        // Slide the window
+        for (int i = windowSize; i < s2.length(); i++) {
+            freq2[s2.charAt(i) - 'a']++;                 // add new char
+            freq2[s2.charAt(i - windowSize) - 'a']--;   // remove old char
+
+            if (matches(freq1, freq2)) return true;
+        }
+
+        return false;
+    }
+
+    private boolean matches(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+}
