d4e9f63948981243ec932ffb7f3b571a2c15fefd
Shantanu Marik <146924611+Shantanum0701@users.noreply.github.com>
2026-02-01 19:55:08 +0530

    Time: 0 ms (100%), Space: 43.3 MB (73.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,15 +2,14 @@ class Solution {
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] < 9) {
-                digits[i]++;   // just add 1 if no carry needed
+                digits[i]++;   
                 return digits;
             }
-            digits[i] = 0;  // if digit was 9, set to 0 and carry continues
+            digits[i] = 0;  
         }
         
-        // If we are here, it means all digits were 9 (e.g., 999 -> 1000)
         int[] res = new int[digits.length + 1];
-        res[0] = 1;  // first digit will be 1, rest will be 0 by default
+        res[0] = 1;  
         return res;
     }
 }
